e2c75f5c710d9abf193f3ebf70052367fe09a0f1
Facundo Jaton <facundojaton@alu.frp.utn.edu.ar>
2021-10-25 20:03:29 -0300

    Tablayout and icons

--- app/src/main/java/com/facundojaton/marvelcomicschallenge/ui/home/HomeFragment.kt
+++ app/src/main/java/com/facundojaton/marvelcomicschallenge/ui/home/HomeFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import com.facundojaton.marvelcomicschallenge.R
 import com.facundojaton.marvelcomicschallenge.databinding.FragmentHomeBinding
 import com.facundojaton.marvelcomicschallenge.ui.adapters.ViewPagerAdapter
 import com.facundojaton.marvelcomicschallenge.ui.characters_list.CharactersFragment
 import com.facundojaton.marvelcomicschallenge.ui.events_list.EventsFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
@@ -41,5 +44,12 @@ class HomeFragment : Fragment() {
         )
 
         binding.vpHome.adapter = adapter
+
+        binding.tabLayout
+
+        TabLayoutMediator(binding.tabLayout, binding.vpHome) { tab, position ->
+            if (position == 0) tab.setCustomView(R.layout.custom_tab_view_item)
+            else tab.setCustomView(R.layout.custom_tab_view_item_calendar)
+        }.attach()
     }
 }
